95460ea79b0843cd64c3cd073304a673ae222d4b
Ajay Sharma <itsecty.ajay@gmail.cvom>
2024-08-25 15:06:06 +0530

    hello and variable code

---
+++ hello/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello, World!")
+	var intNum int
+	intNum += 1
+	fmt.Println(intNum)
+
+	var floatNum float32
+	floatNum += 2
+	println("Number is :", floatNum)
+
+	
+
+
+}
